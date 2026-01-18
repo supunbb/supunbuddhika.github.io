b39e6dbd191fbad8d1114cb0787d43828d6bfee3
supunbb <125901930+supunbb@users.noreply.github.com>
2026-01-18 22:18:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // because this is a user page: supunbuddhika.github.io
   plugins: [react()],
 })
